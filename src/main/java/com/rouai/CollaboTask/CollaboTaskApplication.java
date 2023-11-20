4655243ce0178608fddd5d58470ec2958095d5a6
ChaimaaRO <chaimaa_rouai@um5.ac.ma>
2023-11-20 06:42:39 +0100

    setup project

---
+++ src/main/java/com/rouai/CollaboTask/CollaboTaskApplication.java
@@ -0,0 +1,13 @@
+package com.rouai.CollaboTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaboTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rouai/CollaboTask/CollaboTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rouai.CollaboTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
